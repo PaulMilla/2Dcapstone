d2d9007bd40911dc27b4b6acb350b9954ba53f45
Kevin Wade <wade.kevina@gmail.com>
2014-09-05 16:03:33 -0500

    Super basic character movement

---
+++ Capstone/Assets/PlayerInput.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+	[SerializeField]
+	private float timeBetweenUpdates;
+	private float timeSinceUpdate { get; set; }
+
+	private PlayerModel playerModel { get; set; }
+
+	void Awake() {
+		playerModel = gameObject.GetComponent<PlayerModel>() as PlayerModel;
+	}
+	void FixedUpdate() {
+		if (Time.time > timeBetweenUpdates + timeSinceUpdate) {
+			timeSinceUpdate = Time.deltaTime;
+			Vector2 direction = GetInputDirection();
+			playerModel.Move(direction);
+		}
+	}
+
+	Vector2 GetInputDirection() {
+		Vector2 direction = Vector2.zero;
+		if (Input.GetKey(KeyCode.W)) {
+			direction.y = 1;
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			direction.y = -1;
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			direction.x = -1;
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			direction.x = 1;
+		}
+		return direction.normalized;
+	}
+}
---
+++ Capstone/Assets/PlayerModel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerModel : MonoBehaviour {
+	[SerializeField]
+	private float movementSpeed;
+
+	public void Move(Vector2 direction)
+	{
+		transform.rigidbody.MovePosition(transform.position + (Vector3)(direction * movementSpeed * Time.fixedDeltaTime));
+	}
+}
