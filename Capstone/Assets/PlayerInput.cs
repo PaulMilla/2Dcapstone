dae52a070888f5e3d9f0b5689cd57961b502115d
Jordan Graves <j.l.graves03@gmail.com>
2014-09-06 09:59:59 -0500

    using events for recording

--- Capstone/Assets/PlayerInput.cs
+++ Capstone/Assets/PlayerInput.cs
@@ -4,14 +4,36 @@ using System.Collections;
 public class PlayerInput : MonoBehaviour {
 	
 	private PlayerModel playerModel { get; set; }
+	private Recording2 recording = new Recording2();
+	public RecordedInput hologram;
 
 	void Awake() {
 		playerModel = gameObject.GetComponent<PlayerModel>() as PlayerModel;
 	}
 	void FixedUpdate() {
-			Vector2 direction = GetInputDirection();
-			RecordingManager.Instance.RecordMovement(direction);
-			playerModel.Move(direction);
+		Vector2 direction = GetInputDirection();
+		RecordingManager.Instance.RecordMovement(direction);
+		playerModel.Move(direction);
+		Record();
+	}
+
+	void Record() {
+		RecordedEvent recordedEvent = new RecordedEvent();
+		if (Input.GetKey(KeyCode.W)) {
+			Debug.Log ("Adding 'W'");
+			recordedEvent.AddKeyDown(KeyCode.W);
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			recordedEvent.AddKeyDown(KeyCode.S);
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			recordedEvent.AddKeyDown(KeyCode.A);
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			Debug.Log ("Adding 'D'");
+			recordedEvent.AddKeyDown(KeyCode.D);
+		}
+		recording.AddEvent(recordedEvent);
 	}
 
 	Vector2 GetInputDirection() {
@@ -27,6 +49,12 @@ public class PlayerInput : MonoBehaviour {
 		}
 		if (Input.GetKey(KeyCode.D)) {
 			direction.x = 1;
+		}
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			// Create a new hologram with recordedInput as controller
+			RecordedInput hologramCharacter = (RecordedInput) Instantiate(hologram, new Vector3(0, 0, this.transform.position.z), transform.rotation);
+			Debug.Log ("Creating hologram character with " + recording.NumEvents() + " events ");
+			hologramCharacter.recording = recording;
 		}
 		return direction.normalized;
 	}
