a64352b9c2267a8ec12fc35b45fb85c76753ff0d
Jordan Graves <j.l.graves03@gmail.com>
2014-09-07 09:31:27 -0500

    playing with buttons and doors

---
+++ Capstone/Assets/Door.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+	
+
+	public void Open() {
+		renderer.enabled = false;
+		collider.enabled = false;
+	}
+
+	public void Close() {
+		renderer.enabled = true;
+		collider.enabled = true;
+	}
+}
