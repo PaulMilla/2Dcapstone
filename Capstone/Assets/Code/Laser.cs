85fd6fe0486326679dd129353f44e8defc67ec61
Kevin Wade <wade.kevina@gmail.com>
2014-09-22 14:59:54 -0500

    fixed bug with lasers and switches

--- Capstone/Assets/Code/Laser.cs
+++ Capstone/Assets/Code/Laser.cs
@@ -16,10 +16,8 @@ public class Laser : Activatable {
 	// Update is called once per frame
 	void Update () {
 		if (!Activated) {
-			lineRenderer.SetWidth(0, 0);
 			return;
 		}
-		Debug.Log(transform.right);
 		Ray ray = new Ray(transform.position, transform.right);
 		RaycastHit hit;
 		if (Physics.Raycast(ray, out hit, maxDistance)) {
@@ -30,5 +28,13 @@ public class Laser : Activatable {
 				currentTarget.SendMessageUpwards("Hit", killTime, SendMessageOptions.DontRequireReceiver);
 			}
 		}
+	}
+	public override void Activate() {
+		base.Activate();
+		lineRenderer.SetWidth(0.1f, 0.1f);
+	}
+	public override void Deactivate() {
+		base.Deactivate();
+		lineRenderer.SetWidth(0, 0);
 	}
 }
