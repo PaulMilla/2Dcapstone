27eb6c92c5559972fc455940a9490706ba539fbf
jordan <j.l.graves03@gmail.com>
2014-09-29 20:34:21 -0500

    add light effect on Interactable mouse over

---
+++ Capstone/Assets/CursorController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorController : MonoBehaviour {
+
+	public LensFlare flarePrefab;
+	private LensFlare flare;
+	
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, 100, 1 << LayerMask.NameToLayer ("Interactable"))) {
+			if (flare == null) {
+				Debug.Log("Set Flare");
+				flare = Instantiate(flarePrefab, hit.point, Quaternion.identity) as LensFlare;
+			} else {
+				flare.transform.position = hit.point;
+			}
+		} else {
+			if (flare != null) {
+				GameObject.Destroy(flare.gameObject);
+			}
+		}
+	}
+}
