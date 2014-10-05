463d7500bed04b15b56c70d9e94be0c55673f07d
jordan <j.l.graves03@gmail.com>
2014-10-05 09:32:55 -0500

    add hide objects script to camer

---
+++ Capstone/Assets/Code/HideObjects.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HideObjects : MonoBehaviour {
+
+	private Transform prevTransObject;
+	public Color alphaValue = 0.5f; // our alpha value
+	public List<int> transparentLayers = new List<int>();   // transparency layers.
+	public float maxDistance = 20f; // Max distance from target to camera object.
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
