9f23289c38bcabf6c995487c9728fd790393fe02
jordan <j.l.graves03@gmail.com>
2014-10-09 23:39:00 -0500

    add enery door

---
+++ Capstone/Assets/Code/EnergyDoor.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnergyDoor : Door
+{	
+	private int imagesIndex = 1;
+	public Texture[] images;
+	private int direction = 1;
+	void LateUpdate() 
+	{
+		if (renderer.enabled) {
+			imagesIndex += direction;
+			if (imagesIndex >= images.Length-1 || imagesIndex <= 0) {
+				direction = -direction;
+			}  	 
+			renderer.material.mainTexture = images[imagesIndex];
+		} 
+	}
+}
+
