8620337c2cac02e89f40badb3f29a23d032e08ff
Jordan Graves <j.l.graves03@gmail.com>
2014-10-12 21:52:18 -0500

    hookup dialogue

---
+++ Capstone/Assets/SoundManagerLevel1.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManagerLevel1 : MonoBehaviour {
+
+	AudioSource current;
+
+	public AudioSource welcome;
+	public AudioSource alert;
+
+	public float time_;
+
+	// Use this for initialization
+	void Start () {
+		current = welcome;
+	}
+
+	float rewindTime = 0.0f;
+	float stopTime;
+	bool rewinding = false;
+
+	// Update is called once per frame
+	void Update () {
+		if (current != null && current.isPlaying) {
+			if (Input.GetKeyDown(KeyCode.R)) {
+				current.pitch = -1f;
+			} else if (Input.GetKeyUp(KeyCode.R)) {
+				current.pitch = 1f;
+			} else if (Input.GetKey (KeyCode.R)) {
+				if (current.time <= 0.1f) {
+					current.pitch = 0f;
+				}
+			}
+			time_ = current.time;
+		}
+	}
+
+	void StartClock() {
+
+	}
+
+	void StopClock() {
+
+	}
+}
