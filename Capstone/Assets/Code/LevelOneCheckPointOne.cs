38273e3ae977cb56c30709f4857248cbe1cc4ef5
jordan <j.l.graves03@gmail.com>
2014-10-19 01:06:52 -0500

    add script to tutorial

---
+++ Capstone/Assets/Code/LevelOneCheckPointOne.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelOneCheckPointOne : MonoBehaviour {
+
+	public Camera MainCamera;
+
+	Vector3 cameraInitialPosition;
+	public Transform cameraStopTransform;
+
+	bool triggered = false;
+	bool notDone = true;
+
+	public float cameraMoveSpeed;
+	public PlayerMovement player;
+
+	enum STAGE {
+		STAGE_ONE,
+		STAGE_TWO,
+		STAGE_FINISH
+	}
+
+	private STAGE currentStage = STAGE.STAGE_ONE;
+
+	private float timer = 0.0f;
+	private float stageOneTime = 3.0f;
+	private float stageTwoTime = 3.0f;
+
+	// Update is called once per frame
+	void Update () {
+		if (triggered) {
+			timer += Time.deltaTime;
+			switch (currentStage) {
+			case STAGE.STAGE_ONE:
+
+				StageOneUpdate();
+				break;
+			case STAGE.STAGE_TWO:
+				StageTwoUpdate();
+				break;
+			case STAGE.STAGE_FINISH:
+				FinishCheckpoint();
+				break;
+			}
+		}
+	}
+
+	void StageOneUpdate() {
+		Debug.Log("Stage One");
+		if (timer >= stageOneTime) {
+			currentStage = STAGE.STAGE_TWO;
+			timer = 0.0f;
+		}
+		MainCamera.transform.position = Vector3.Lerp(MainCamera.transform.position, cameraStopTransform.position, cameraMoveSpeed);
+		player.GetAgent().Stop();
+	}
+
+	void StageTwoUpdate() {
+		Debug.Log("Stage Two");
+		player.GetAgent().Stop();
+		if (timer >= stageTwoTime) {
+			currentStage = STAGE.STAGE_FINISH;
+			timer = 0.0f;
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag.Equals("Player")) {
+			Debug.Log("Player Entered");
+			if (!triggered) {
+				currentStage = STAGE.STAGE_ONE;
+				triggered = true;
+				player.enabled = false;
+				player.movementEnabled = false;
+				player.GetAgent().Stop();
+				cameraInitialPosition = MainCamera.transform.position;
+				MainCamera.GetComponent<CameraFollow>().enabled = false;
+			}
+		}
+	}
+
+	void FinishCheckpoint() {
+		Debug.Log("Stage Finish");
+		//MainCamera.transform.position = Vector3.Lerp(MainCamera.transform.position, cameraInitialPosition, cameraMoveSpeed);
+		player.enabled = true;
+		player.movementEnabled = true;
+		MainCamera.GetComponent<CameraFollow>().enabled = true;
+	}
+}
---
+++ Capstone/Assets/Code/SoundManagerLevel1.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManagerLevel1 : MonoBehaviour {
+
+	AudioSource current;
+
+	public AudioSource welcome;
+	public AudioSource alert;
+
+	public float time_;
+
+	bool triggered = false;
+
+	// Use this for initialization
+	void Start () {
+		current = welcome;
+	}
+
+	float rewindTime = 0.0f;
+	float stopTime;
+	bool rewinding = false;
+
+	// Update is called once per frame
+	void Update () {
+		if (current != null && current.isPlaying) {
+			if (Input.GetKeyDown(KeyCode.R)) {
+				current.pitch = -1f;
+			} else if (Input.GetKeyUp(KeyCode.R)) {
+				current.pitch = 1f;
+			} else if (Input.GetKey (KeyCode.R)) {
+				if (current.time <= 0.1f) {
+					current.pitch = 0f;
+				}
+			}
+			time_ = current.time;
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (triggered) {
+			return;
+		}
+		if (other.tag.Equals("Player")) {
+			triggered = true;
+			if (current != null) {
+				current.Stop();
+				current = alert;
+				alert.Play();
+			}
+		}
+	}
+
+	void Pause() {
+		if (current != null) {
+			current.Pause();
+		}
+	}
+
+	void Resume() {
+		if (current != null) {
+			current.Play();
+		}
+	}
+
+}
