07e5eacacc3f42bded931d61550c1bb2551f609f
pmilla <paul.milla1@utexas.edu>
2014-10-20 20:42:55 -0500

    Serialized a TextEvent class

---
+++ Capstone/Assets/Code/GUITextBox.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GUITextBox : MonoBehaviour {
+    private Animator anim;
+    private Text text;
+    public Collider playerCollider;
+    public TextEvent[] textEvents;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        text = GetComponentInChildren<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    [System.Serializable]
+    public class TextEvent
+    {
+        public string text;
+        public Collider collider;
+    }
+}
