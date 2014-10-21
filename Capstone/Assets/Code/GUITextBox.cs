7c72f684c4c9f07907ea6ce79587e5a021e0d46a
pmilla <paul.milla1@utexas.edu>
2014-10-21 01:53:14 -0500

    Implemented TextEvents
    Under Prefabs/TextEvents
    All they are is a Box Collider with an OnTriggerEnter function that the
    message dialog box with the given string whenever the player enters the
    collider zone

--- Capstone/Assets/Code/GUITextBox.cs
+++ Capstone/Assets/Code/GUITextBox.cs
@@ -3,26 +3,39 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class GUITextBox : MonoBehaviour {
-    private Animator anim;
-    private Text text;
-    public Collider playerCollider;
-    public TextEvent[] textEvents;
+    private static Animator dialog;
+    private static Text text;
+    private static float showTime;
 
 	// Use this for initialization
 	void Start () {
-        anim = GetComponent<Animator>();
+        dialog = GetComponent<Animator>();
         text = GetComponentInChildren<Text>();
+        showTime = 0.0f;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public static void ShowMessageForDuration(string message, float duration) {
+        text.text = message;
+        showTime = duration;
+        if (showTime > 0.0f) {
+            Show();
+        }
+    }
 
-    [System.Serializable]
-    public class TextEvent
-    {
-        public string text;
-        public Collider collider;
+    public static void Hide() {
+        dialog.Play("GUITextBox_Hide");
+    }
+
+    public static void Show() {
+        dialog.Play("GUITextBox_Show");
+    }
+
+    void FixedUpdate() {
+        if (showTime > 0.0f) {
+            showTime -= Time.fixedDeltaTime;
+            if (showTime <= 0.0f) {
+                Hide();
+            }
+        }
     }
 }
