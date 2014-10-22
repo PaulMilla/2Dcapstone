0f9e7893a781ba01fa1454f0b4efbde9e55f7731
Paul Milla <paul.milla1@utexas.edu>
2014-10-22 17:10:34 -0500

    Got a single footstep to fade in and out depending on weather the rewind button is pressed

---
+++ Capstone/Assets/Footsteps.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Footsteps : MonoBehaviour {
+	static float time;
+
+	// Use this for initialization
+	void Start () {
+		//TODO: Separate this out better and/or find our previous implementation
+		time = GameTime.gameTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public class GameTime {
+		public static float gameTime;
+
+		void Start() {
+			gameTime = 0;
+		}
+
+		void Update() {
+			if(Input.GetButtonDown ("Rewind")) {
+				--gameTime;
+			} else {
+				++gameTime;
+			}
+		}
+	}
+}
