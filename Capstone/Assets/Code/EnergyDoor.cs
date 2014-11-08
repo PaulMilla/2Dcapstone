bc45a5663afd995ec477a0df1c1cbe1ff9fa5dcb
jordan <j.l.graves03@gmail.com>
2014-11-08 12:25:18 -0600

    add alpha to door

--- Capstone/Assets/Code/EnergyDoor.cs
+++ Capstone/Assets/Code/EnergyDoor.cs
@@ -7,22 +7,36 @@ public class EnergyDoor : Door
 	public Texture[] images;
 	private int direction = 1;
 
+	private float dissolveAmount = 0.0f;
+	private const float dissolveSpeed = 0.04f;
+
 	AudioSource audioActivate;
 	AudioSource audioDeactivate;
-
+	
 	void Awake() {
 		audioActivate = transform.Find ("AudioActivate").gameObject.GetComponent<AudioSource> ();
 		audioDeactivate = transform.Find ("AudioDeactivate").gameObject.GetComponent<AudioSource> ();
 	}
 
+	void Update() {
+		if (this.Activated) {
+			dissolve ();
+		} else {
+			appear();
+		}
+	}
+
+
+
+
 	protected override void Activate() {
-		renderer.enabled = false;
+		//renderer.enabled = false;
 		collider.enabled = false;
 		audioActivate.Play ();
 	}
 	
 	protected override void Deactivate() {
-		renderer.enabled = true;
+		//renderer.enabled = true;
 		collider.enabled = true;
 		audioDeactivate.Play ();
 	}
@@ -36,6 +50,18 @@ public class EnergyDoor : Door
 			}  	 
 			renderer.material.mainTexture = images[imagesIndex];
 		} 
+	}
+
+	private void dissolve() {
+		dissolveAmount += dissolveSpeed;
+		dissolveAmount = Mathf.Clamp(dissolveAmount, 0.0f, 1.0f);
+		this.renderer.material.SetFloat ("_SliceAmount", dissolveAmount);
+	}
+
+	private void appear() {
+		dissolveAmount -= dissolveSpeed;
+		dissolveAmount = Mathf.Clamp(dissolveAmount, 0.0f, 1.0f);
+		this.renderer.material.SetFloat ("_SliceAmount", dissolveAmount);
 	}
 }
 
