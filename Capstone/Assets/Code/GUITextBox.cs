4ba824f8177506a6f23582806890bb49dd341d9b
Paul Milla <paul.milla1@utexas.edu>
2014-11-17 15:28:28 -0600

    Updated GUITextBox to act like System Message

--- Capstone/Assets/Code/GUITextBox.cs
+++ Capstone/Assets/Code/GUITextBox.cs
@@ -3,39 +3,55 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class GUITextBox : MonoBehaviour {
-    private Animator dialog;
     private Text text;
+    private Animator dialog;
+	private bool isTimed;
     private float showTime;
+	public bool clickToGoAway {get;set;}
 
 	// Use this for initialization
 	void Start () {
         dialog = GetComponent<Animator>();
         text = GetComponentInChildren<Text>();
+		isTimed = false;
         showTime = 0.0f;
 	}
-	
+
+	public void ShowMessage(string message) {
+		if (text == null) return;
+		text.text = message;
+		Show();
+	}
+
     public void ShowMessageForDuration(string message, float duration) {
 		if (text == null) return;
         text.text = message;
         showTime = duration;
+		isTimed = true;
         if (showTime > 0.0f) {
-            Show();
+			Show();
         }
     }
 
-    public void Hide() {
-        dialog.Play("GUITextBox_Hide");
-    }
+	public void Clicked() {
+		if (clickToGoAway) 
+			Hide();
+	}
 
-    public void Show() {
-        dialog.Play("GUITextBox_Show");
-    }
+	public void Show() {
+		dialog.Play("GUITextBox_Show");
+	}
+
+	public void Hide() {
+		dialog.Play("GUITextBox_Hide");
+	}
 
     void FixedUpdate() {
-        if (showTime > 0.0f) {
+        if (isTimed && showTime > 0.0f) {
             showTime -= Time.fixedDeltaTime;
             if (showTime <= 0.0f) {
-                Hide();
+				Hide();
+				isTimed = false;
             }
         }
     }
