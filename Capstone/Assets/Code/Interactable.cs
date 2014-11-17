2ed30d95e4a73342267572de9866ebb810bbc67b
Kyle Nielsen <nielsen.kyle93@utexas.edu>
2014-11-16 19:16:43 -0600

    Deleted stuff

--- Capstone/Assets/Code/Interactable.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public abstract class Interactable : MonoBehaviour {
-    public abstract void OnInteract();
-    public abstract bool InPositionToInteract(CharacterMovement character);
-    public abstract bool TryInteract(CharacterMovement character);
-}
