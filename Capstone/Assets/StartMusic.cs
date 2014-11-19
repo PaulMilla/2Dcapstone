99cc9b3f2140d2b0749d9035c9d1e97ab6f20ca4
jordan <j.l.graves03@gmail.com>
2014-11-18 21:39:17 -0600

    add movie, music, flow to StartScreen

---
+++ Capstone/Assets/StartMusic.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMusic : MonoBehaviour {
+	
+	// Use this for initialization
+	void Awake() {
+		DontDestroyOnLoad (this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void OnLevelWasLoaded (int level) {
+		Debug.Log ("LEVEL: " + Application.loadedLevelName);
+		if( !(Application.loadedLevelName.Equals("StartScreen") || Application.loadedLevelName.Equals("Briefing")) ) {
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Capstone/Assets/StartScreen.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreen : MonoBehaviour {
+
+	public MovieTexture movieTexture;
+	public Texture playButton;
+
+	// Use this for initialization
+	void Start () {
+		movieTexture.Play ();
+		movieTexture.loop = true;
+	}
+	
+	// Update is called once per frame
+	void OnGUI() {
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), movieTexture, ScaleMode.StretchToFill);
+		Rect r = new Rect (Screen.width / 2 - playButton.width / 4, Screen.height / 2 + playButton.height / 3, playButton.width / 2, playButton.height / 2);
+		GUI.DrawTexture (r, playButton);
+	
+
+		if (GUI.Button(r , "", new GUIStyle()))
+		{
+			Application.LoadLevel("Briefing");
+		}
+
+	}
+
+}
